55110ad2374510754f3e130c48ae790194701796
arslanansari-29 <shamim0951@gmail.com>
2025-10-05 00:35:48 +0530

    git add. git commit -m "Fixed resource leaks and improved code readability in Java Projects." git push origin main

--- Java Projects/Utils.java
+++ Java Projects/Utils.java
@@ -3,5 +3,51 @@ public class Utils {
         return String.valueOf(Math.abs(number)).length();
     }
 
-    
+    public static int reverse(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            reversed = reversed * 10 + (number % 10);
+            number /= 10;
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(int number) {
+        return number == reverse(number);
+    }
+
+    public static int factorial(int number) {
+        if (number < 0) throw new IllegalArgumentException("Number must be non-negative.");
+        int fact = 1;
+        for (int i = 2; i <= number; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static int sumOfDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    public static int productOfDigits(int number) {
+        int product = 1;
+        while (number != 0) {
+            product *= number % 10;
+            number /= 10;
+        }
+        return product;
+    }
 }
