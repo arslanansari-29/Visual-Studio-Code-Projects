2a6590365ccd561b550df4c6affcf8fcaff0a7de
arslanansari-29 <shamim0951@gmail.com>
2026-03-29 16:01:17 +0530

    git add. git commit -m "Updated RandomExample.java with new random number generation logic." git push origin main

--- Java Projects/11th/RandomExample.java
+++ Java Projects/11th/RandomExample.java
@@ -5,6 +5,13 @@ public class RandomExample {
         Random random = new Random();
         int randomNumber = random.nextInt(1,7);
         System.out.println("Random number: " + randomNumber);
+        // Take input Unique(No Duplicate) Random elements in an array using random class
+        int[] arr = new int[5];
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = random.nextInt(1,11);
+        }
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
-    
 }
