d82c46d87a479e466dfb0cde0b366b356eb56385
arslanansari-29 <shamim0951@gmail.com>
2026-04-12 11:13:06 +0530

    git add. git commit -m "Updated Prog2.java and launch.json" git push origin main

--- Java Projects/12th/Lab/Prog2.java
+++ Java Projects/12th/Lab/Prog2.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
-
-public class Prog2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Pangram {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
         System.out.print("INPUT: ");
-        String s = sc.nextLine();
-        if(".!?".indexOf(s.charAt(s.length()-1))==-1) {
+        String s = in.nextLine().toLowerCase();
+        char last = s.charAt(s.length() - 1);
+        System.out.print("OUTPUT: ");
+        if(".?!".indexOf(last) == -1){
             System.out.println("INVALID INPUT");
-            System.exit(0);
+            return;
         }
-        // System.out.println("Its there");
-        for(char ch = 'a'; ch<='z'; ch++) {
-            
+        int missing = 0;
+        String m = "";
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            if(s.indexOf(ch) == -1){
+                missing++;
+                m += ch + " ";
+            }
+        }
+        if(missing == 0)
+            System.out.println("IT IS A PANGRAM");
+        else if(missing == 1){
+            System.out.println("PANGRAMMATIC LIPOGRAM");
+            System.out.println("MISSING: " + m);
+        }
+        else{
+            System.out.println("NEITHER");
+            System.out.println("MISSING: " + m);
         }
     }
 }
